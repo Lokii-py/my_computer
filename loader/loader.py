2e484c0b2b8df04fd1790b2a534ccad219380086
Lokesh Das <thelokesh08@gmail.com>
2026-08-02 18:10:16 -0500

    Another architecture that can load the machine code to CPU memory

---
+++ loader/loader.py
@@ -0,0 +1,10 @@
+class Loader:
+    def __init__(self):
+        pass
+
+    def load(self, cpu, translator_out: list[tuple], start_addr: int = 0):
+        """Load the instruction converted from assembly to cpu parsable instruction"""
+        for instruction in translator_out:
+            cpu.memory[start_addr] = instruction
+            start_addr += 1
+        
