2f718d9d26d490892ee6e1cd538b5f7ed9a44592
Lokesh Das <thelokesh08@gmail.com>
2026-08-06 21:22:10 -0500

    program counter was changed but PC remained at same location causing problem for IR

--- loader/loader.py
+++ loader/loader.py
@@ -4,6 +4,7 @@ class Loader:
 
     def load(self, cpu, translator_out: list[tuple], start_addr: int = 0):
         """Load the instruction converted from assembly to cpu parsable instruction"""
+        cpu.pc = start_addr
         for instruction in translator_out:
             cpu.memory[start_addr] = instruction
             start_addr += 1
