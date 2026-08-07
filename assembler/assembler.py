b2bb2d9e3adc14434bbf4984f753698a8d02b6c8
Lokesh Das <thelokesh08@gmail.com>
2026-08-07 00:28:07 -0500

    Added program cleaning; able to handles asm file

--- assembler/assembler.py
+++ assembler/assembler.py
@@ -1,15 +1,15 @@
 class Assembler:
     def __init__(self):
-        self.symbol = {
-            "counter": 40,
-            "one": 41,
-        }
+        self.symbol = {}
+        self.data_segment = {}
 
-    def assemble(self, program: list[str]):
+    def assemble(self, program: list[str]) -> list[tuple]:
         """Translate the program to CPU accepted format"""
         machine_code: list[tuple] = []
 
-        for instruction in program:
+        cleaned_program = self.clean_program(program)
+
+        for instruction in cleaned_program:
 
             parts = instruction.split(" ")
             if len(parts) == 2:
@@ -22,11 +22,59 @@ class Assembler:
             operand = int(operand) if operand is not None else operand
 
             machine_code.append((op_code, operand))
-        return machine_code
+
+        return machine_code, self.data_segment
+
+    def clean_program(self, lines: list[str]) -> list[str]:
+        """Clean the assembler format program"""
+        if not lines:
+            raise ValueError("No program to run. The file is empty")
+
+        inter_program = []
+        for line in lines:
+            clean_line = line.strip()
+
+            if clean_line == "":
+                continue
+
+            if clean_line.startswith("#"):
+                continue
+
+            clean_line = clean_line.split("#")[0].strip()
+            inter_program.append(clean_line)
+
+        self.create_symbol(inter_program)
+
+        cleaned_program = []
+        for line in inter_program:
+            if ":" not in line:
+                cleaned_program.append(line)
+
+        return cleaned_program
+
+    def create_symbol(self, lines: list[str], mem_addr=40) -> None:
+        """Create a symbol for operand look up"""
+        count = 0
+        for line in lines:
+            count += 1
+            if ":" in line:
+                count -= 1
+                x, y = line.split(":")
+                x, y = x.strip(), y.strip()
+                if y:
+                    _, val = y.split()
+                    val = int(val)
+
+                    self.symbol[x] = mem_addr
+                    self.data_segment[mem_addr] = val
+
+                    mem_addr += 1
+                else:
+                    self.symbol[x] = count
 
     def resolve_operand(self, operand):
         """Resolve the Operand into number so CPU can understand it"""
-        operand = operand.strip().lower()
+        operand = operand.strip()
         if operand.isdigit():
             return operand
         return self.symbol[operand]
